12a7caf592ff496c732b21b06fd4a7b8dacca108
Kim Samba <okabe.kimtaro@gmail.com>
2022-11-12 19:02:52 -0500

    fix: remove tfsec errors

--- main.tf
+++ main.tf
@@ -6,8 +6,10 @@ resource "random_string" "this" {
 
 resource "random_pet" "this" {}
 
+   #tfsec:ignore:google-storage-bucket-encryption-customer-key
 resource "google_storage_bucket" "this" {
   name          = "${random_string.this.id}${random_string.this.result}-${var.environment}"
   location      = var.location
+  uniform_bucket_level_access = true
   force_destroy = true
 }
