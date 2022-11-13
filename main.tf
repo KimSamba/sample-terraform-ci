4ce1776b6fbc0affb28c7c9b17f65228182b3073
Kim Samba <okabe.kimtaro@gmail.com>
2022-11-12 19:17:54 -0500

    ci: add checkov in workflow

--- main.tf
+++ main.tf
@@ -8,8 +8,12 @@ resource "random_pet" "this" {}
 
 #tfsec:ignore:google-storage-bucket-encryption-customer-key
 resource "google_storage_bucket" "this" {
+  #checkov:skip=CKV_GCP_62:The bucket is a public static content host
   name                        = "${random_string.this.id}${random_string.this.result}-${var.environment}"
   location                    = var.location
   uniform_bucket_level_access = true
   force_destroy               = true
+  versioning                  = {
+    enabled = true
+  }
 }
